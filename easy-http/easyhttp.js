92b4d7af1844e16615a6afb53623b61cae5b46dd
the-markussohn <markus.justinas@gmail.com>
2018-08-26 21:44:38 +0300

    POST, PUT, DELETE

--- easy-http/easyhttp.js
+++ easy-http/easyhttp.js
@@ -19,9 +19,43 @@ EasyHttp.prototype.get = function (url, callback) {
 };
 
 // POST Request
+EasyHttp.prototype.post = function (url, data, callback) {
+	this.http.open('POST', url, true);
+	this.http.setRequestHeader('Content-Type', 'application/json');
+	
+	let self = this;
+	this.http.onload = function(){
+		callback(null, self.http.response);
+	};
 
+	this.http.send(JSON.stringify(data));
+}
 
 // PUT Request
+EasyHttp.prototype.put = function (url, data, callback) {
+	this.http.open('PUT', url, true);
+	this.http.setRequestHeader('Content-Type', 'application/json');
 
+	let self = this;
+	this.http.onload = function () {
+		callback(null, self.http.response);
+	};
+
+	this.http.send(JSON.stringify(data));
+}
 
 // DELETE Request
+EasyHttp.prototype.delete = function (url, callback) {
+	this.http.open('DELETE', url, true);
+
+	let self = this;
+	this.http.onload = function () {
+		if (self.http.status === 200) {
+			callback(null, 'Post Deleted');
+		} else {
+			callback(`Error: ${self.http.status}`);
+		}
+	};
+
+	this.http.send();
+};
