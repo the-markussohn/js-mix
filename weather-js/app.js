cc917611e790b91f2258121c707ecdb00c4cadf6
the-markussohn <markus.justinas@gmail.com>
2018-08-27 13:22:52 +0300

    change city

--- weather-js/app.js
+++ weather-js/app.js
@@ -2,6 +2,12 @@ const weather = new Weather('Vilnius');
 const ui = new UI();
 
 document.addEventListener('DOMContentLoaded', getWeather);
+document.querySelector('#w-change-btn').addEventListener('click', () => {
+    const city = document.querySelector('#city').value;
+    weather.changeLocation(city);
+    getWeather();
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
