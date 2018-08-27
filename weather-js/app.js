f825bedcb030c108efacfa91839a338955f462ee
the-markussohn <markus.justinas@gmail.com>
2018-08-27 13:31:51 +0300

    city in storage

--- weather-js/app.js
+++ weather-js/app.js
@@ -1,10 +1,14 @@
-const weather = new Weather('Vilnius');
 const ui = new UI();
+const storage = new Storage();
+
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city);
 
 document.addEventListener('DOMContentLoaded', getWeather);
 document.querySelector('#w-change-btn').addEventListener('click', () => {
     const city = document.querySelector('#city').value;
     weather.changeLocation(city);
+    storage.setLocationData(city);
     getWeather();
     $('#locModal').modal('hide');
 });
